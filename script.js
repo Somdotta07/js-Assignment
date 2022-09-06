658a5f88522240688a7b6bbf6aff00701b260448
Somdotta07 <jhilick.s@gmail.com>
2022-09-06 13:57:50 +0200

    Added functionality and modal

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
 // Mathematical Shapes
 //a) Write a JavaScript program to find the diagonal of a square where the length of each side is 9.
 
-function findDiagonal() {
-  let sideLength = 9;
-  return Math.sqrt(2) * sideLength;
-}
+// function findDiagonal() {
+//   let sideLength = 9;
+//   return Math.sqrt(2) * sideLength;
+// }
 
-console.log(findDiagonal());
+// console.log(findDiagonal());
 // b) Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6 and 7.
 
-function areaOfTriangle() {
-  let sideA = 5,
-    sideB = 6,
-    sideC = 7;
-  let semiPerimeter = (sideA + sideB + sideC) / 2;
-  let area = 0;
-  area = Math.sqrt(
-    semiPerimeter *
-      (semiPerimeter - sideA) *
-      (semiPerimeter - sideB) *
-      (semiPerimeter - sideC),
-  );
-  const fixedArea = area.toFixed(2);
-  return fixedArea;
-}
+// function areaOfTriangle() {
+//   let sideA = 5,
+//     sideB = 6,
+//     sideC = 7;
+//   let semiPerimeter = (sideA + sideB + sideC) / 2;
+//   let area = 0;
+//   area = Math.sqrt(
+//     semiPerimeter *
+//       (semiPerimeter - sideA) *
+//       (semiPerimeter - sideB) *
+//       (semiPerimeter - sideC),
+//   );
+//   const fixedArea = area.toFixed(2);
+//   return fixedArea;
+// }
 
-console.log(areaOfTriangle());
+// console.log(areaOfTriangle());
 
-const circle = (r) => {
-  let circumference = 0,
-    surfaceArea = 0;
-  const PI = 3.14;
-  circumference = 2 * PI * r;
-  surfaceArea = PI * r ** 2;
-  return [circumference, surfaceArea];
-};
+// const circle = (r) => {
+//   let circumference = 0,
+//     surfaceArea = 0;
+//   const PI = 3.14;
+//   circumference = 2 * PI * r;
+//   surfaceArea = PI * r ** 2;
+//   return [circumference, surfaceArea];
+// };
 
-console.log(circle(4));
+// console.log(circle(4));
 
-// Conditional Statements  & Loops
+// // Conditional Statements  & Loops
 
-//a) Write a JavaScript program that accepts two integers and displays the larger of the two.
+// //a) Write a JavaScript program that accepts two integers and displays the larger of the two.
 
-function condition() {
-  let num1 = 9,
-    num2 = 5;
-  if (num1 > num2) {
-    return num1;
-  }
-}
+// function condition() {
+//   let num1 = 9,
+//     num2 = 5;
+//   if (num1 > num2) {
+//     return num1;
+//   }
+// }
 
-console.log(condition());
+// console.log(condition());
 
 //b) Write a JavaScript program that checks whether an integer is an even or an odd number.
 
@@ -66,12 +66,25 @@ console.log(condition());
 // oddEven()
 /*------------------------------------------------------------------------------------------------------------------------------------------*/
 
-
-
-
-
-
 //(Main JavaScript Project):
+const restart = document.querySelector(".restart-btn");
+const gameBtns = document.querySelectorAll(".choice-btn");
+const scoreBoard = document.querySelector(".score-board");
+const result = document.querySelector(".modal-content");
+const modal = document.querySelector(".modal");
+const score = {
+  playerScore: 0,
+  computerScore: 0,
+};
+
+function game(e) {
+  restart.style.display = "inline-block";
+  const playerSelection = e.target.id.toUpperCase();
+  const computerSelection = computerPlay().toUpperCase();
+  const winner = playRound(playerSelection, computerSelection);
+  finalWinner(winner, computerSelection);
+}
+
 function computerPlay() {
   let options = ["Rock", "Paper", "Scissors"];
   let random = Math.floor(Math.random() * 3);
@@ -100,51 +113,46 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let computerScore = 0,
-    playerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const computerSelection = computerPlay().toUpperCase();
-    // const playerSelection = prompt().toUpperCase();
-    let gameRound = playRound(playerSelection, computerSelection);
-    console.log(gameRound);
-    let winner = "Computer Win!";
-    let winnerP = "Player Win!";
-    if (gameRound === winner) {
-      computerScore++;
-      console.log(`Computer score: ${computerScore}`)
-    } else if (gameRound === winnerP) {
-      playerScore++;
-      console.log(`Player score: ${playerScore}`)
-    }
-  }
-  if (computerScore === playerScore) {
-    return "HURRAH!! DRAW!";
-  } else if (computerScore > playerScore) {
-    return "Oh! Computer Win!";
+function finalWinner(winner, computerSelection) {
+  if (winner === "Computer Win!") {
+    score.computerScore++;
+    result.innerHTML = `     
+     <h1 class="text-win"> Computer Win</h1>
+     <img src="./images/${computerSelection}.svg" id= ${computerSelection} />
+            <p>Computer Chose <strong>${computerSelection}</p>`;
+  } else if (winner === "Player Win!") {
+    score.playerScore++;
+    result.innerHTML = `     
+     <h1 class="text-win"> Player Win</h1>
+     <img src="./images/${computerSelection}.svg" id= ${computerSelection} />
+     <p>Computer Chose <strong>${computerSelection}</strong></p>`;
   } else {
-    return "WELL DONE!! Player Win!!";
+    result.innerHTML = `     
+     <h1 class="text-win"> It's Draw!!</h1>
+     <img src="./images/${computerSelection}.svg" id= ${computerSelection} />
+     <p>Computer Chose <strong>${computerSelection}</strong></p>`;
+  }
+  scoreBoard.innerHTML = `
+    <p>Player Score: ${score.playerScore}</p>
+    <p>Computer Score: ${score.computerScore}</p>
+  `;
+  modal.style.display = "block";
+}
+
+function restartGame() {
+  (score.computerScore = 0), (score.playerScore = 0);
+  scoreBoard.innerHTML = `
+    <p>Player Score: 0</p>
+    <p>Computer Score: 0</p>
+  `;
+}
+
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
   }
 }
 
-console.log(game());
-
-
-
-
-/*              */
-
-// const element = document.createElement(<p>"Hey I'm red!"</p>); 
-// element.style.color = 'red';
-// const heading = document.createElement(<h3>"I'm a blue h3!"</h3>); 
-// heading.style.color = 'blue';
-
-// const newDiv = document.createElement("div");
-// const newHeading = document.createElement(<h1>Hello!</h1>)
-// const button = document.createElement(<button>Click me</button>)
-// newDiv.appendChild(newHeading);
-// newDiv.appendChild(button)
-// const main = document.querySelector('#main');
-
-// main.appendChild(newDiv);
+gameBtns.forEach((choice) => choice.addEventListener("click", game));
+window.addEventListener("click", clearModal);
+restart.addEventListener("click", restartGame);
